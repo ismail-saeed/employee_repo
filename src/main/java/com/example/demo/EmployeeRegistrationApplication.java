4e13d098ab3bc3788098dde0fbf0db169314c3bc
Omnia <omniaeid266@gmail.com>
2023-11-27 18:42:46 -0600

    first commit

---
+++ src/main/java/com/example/demo/EmployeeRegistrationApplication.java
@@ -0,0 +1,24 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class EmployeeRegistrationApplication {
+
+	@GetMapping("/get")
+	public String message()
+	{
+		
+		return "Employee created succsfuly";
+	}
+	
+	
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeRegistrationApplication.class, args);
+	}
+
+}
